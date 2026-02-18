059172250bf3b66f98f4d713be5bcdb3c2225ad4
kkatalz <zlata.kiev2004@gmail.com>
2026-02-18 17:05:56 +0200

    feat: create types for Book, User and Loan; add validate function

---
+++ src/types/library.ts
@@ -0,0 +1,28 @@
+export type Book = {
+  id: string;
+  title: string;
+  author: string;
+  year: number;
+  isbn: string;
+  available: boolean;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export type Loan = {
+  id: string;
+  userId: string;
+  bookId: string;
+  loanDate: Date;
+  returnDate: Date | null;
+  status: STATUS;
+};
+
+enum STATUS {
+  ACTIVE = 'ACTIVE',
+  RETURNED = 'RETURNED',
+}
