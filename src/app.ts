832352efaa18a48eb956a7b1112d1daac5ca6e0f
kkatalz <zlata.kiev2004@gmail.com>
2026-02-19 16:30:23 +0200

    feat: add middleware to catch throws from route handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use('/api', routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({ error: err.message });
+});
 
 export default app;
